4d9f67758b48028e9cbfe2843b1ce0d03182a480
Mohamedsabry109 <mohamedsabry109@gmail.com>
2020-03-20 00:33:01 +0200

    adding buffer

--- rlfd.py
+++ rlfd.py
@@ -16,7 +16,9 @@ import pygame
 from Environments import carla_environment
 
 from Agent import dqn
-
+from Memory.memory_buffer import MemoryBuffer
 
 agent = dqn.DDQN()
+target_agent = dqn.DDQN()
+memory_buffer = MemoryBuffer(buffer_size = 1000, with_per = True)
 agent.get_branched_network(input_shape=(3,88,200))
