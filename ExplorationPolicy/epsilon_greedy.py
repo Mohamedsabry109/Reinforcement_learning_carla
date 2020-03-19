ecc4c912c93c33a5f564177bf0b002784c6c3aff
Mohamedsabry109 <mohamedsabry109@gmail.com>
2020-03-20 01:10:00 +0200

    update epsilon greedy

--- ExplorationPolicy/epsilon_greedy.py
+++ ExplorationPolicy/epsilon_greedy.py
@@ -1,32 +1,23 @@
 import random
+import numpy as np
+
 
 class EpsilonGreedy():
-    def __init__(self, epsilon, counts, values):
-        self.epsilon = epsilon # probability of explore
-        self.counts = counts # number of pulls for each arms
-        self.values = values # average amount of reward we've gotten from each arms
-        return
+    def __init__(self, epsilon, linear_schedule = None):
+        self.epsilon = epsilon
+        self.linear_schedule = linear_schedule
+        self.epsilon_counter = 0
 
-    def initialize(self, n_arms):
-        self.counts = [0 for col in range(n_arms)]
-        self.values = [0.0 for col in range(n_arms)]
-        return
-
-    def ind_max(self, x):
-        m = max(x)
-        return x.index(m)
-
-    def select_arm(self):
-        if random.random() > self.epsilon:
-            return self.ind_max(self.values)
+    def choose_action(self,values):
+        if (self.linear_schedule):
+                self.update_epsilon()
+        if random.randint(0,1) > self.epsilon:
+            return np.argmax(values)
         else:
-            return random.randrange(len(self.values))
+            return random.randrange(len(values))
 
-    def update(self, chosen_arm, reward):
-        self.counts[chosen_arm] = self.counts[chosen_arm] + 1
-        n = self.counts[chosen_arm]
+    def update_epsilon(self):
+        if self.epsilon <= self.linear_schedule[2]:  
+            self.epsilon_counter += 1
+            self.epsilon -= (self.linear_schedule[0] - self.linear_schedule[1]) / self.linear_schedule[2] 
 
-        value = self.values[chosen_arm]
-        new_value = ((n-1) / float(n)) * value + (1 / float(n)) * reward # weighted average of the previously estimated value and the reward we just received
-        self.values[chosen_arm] = new_value
-        return
--- rlfd.py
+++ rlfd.py
@@ -17,8 +17,12 @@ from Environments import carla_environment
 
 from Agent import dqn
 from Memory.memory_buffer import MemoryBuffer
+from ExplorationPolicy.epsilon_greedy import EpsilonGreedy
+
+
 
 agent = dqn.DDQN()
 target_agent = dqn.DDQN()
+exp_policy = EpsilonGreedy(epsilon = 0.5, linear_schedule = [0.5,0.05,10])
 memory_buffer = MemoryBuffer(buffer_size = 1000, with_per = True)
 agent.get_branched_network(input_shape=(3,88,200))
